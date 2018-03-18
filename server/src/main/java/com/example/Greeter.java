5ae816f4b8be490c1ae64b2cfd421b3fb9dcef28
patricio20 <npatricio20@gmail.com>
2018-03-18 15:27:53 -0500

    Fix javaDoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,9 +13,9 @@ public class Greeter {
   }
 
   /**
-  * Función que permite saludar
-  * @param someone el nombre de la persona
-  * @return saludo
+  * Función que permite saludar.
+  * @param someone es el nombre de la persona.
+  * @return saludo.
   */
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
